c569c6741ab53be161db7a02da6b5f0d62a28e75
Richard Wutscher <richard.wutscher@gmail.com>
2022-11-04 12:18:06 +0100

    progress

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react'
 import HomePage from '../app/page'
 import '@testing-library/jest-dom'
+import getNotes from '../pages/api/getNotes'
+import { NextApiRequest } from 'next'
 
 describe('Home', () => {
   it('renders a heading', () => {
@@ -12,4 +14,35 @@ describe('Home', () => {
 
     expect(heading)//.toContain(<h1 class="text-3xl font-bold underline">Seas</h1>)
   })
+
+})
+
+describe('GetNotes', () => {
+  it('returns Notes', () => {
+    const req = {
+      body: JSON.stringify({
+        discount: .2,
+        tax: .06,
+        items: [
+          {
+            id: 1,
+            price: 19.99,
+            quantity: 2
+          },
+          {
+            id: 2,
+            price: 43.49,
+            quantity: 1
+          }
+        ]
+      })
+    };
+    const res = {
+      status: () => ({
+        json: () => {}
+      })
+    }
+    getNotes(req, res)
+  })
+
 })
--- playwright/tests/example.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('should navigate to the notes page', async ({ page }) => {
-  await page.goto('/')
-  await page.click('text=Notes')
-  await expect(page).toHaveURL('/notesDirect')
-  await expect(page.locator('h1')).toContainText('Notes')
-})
